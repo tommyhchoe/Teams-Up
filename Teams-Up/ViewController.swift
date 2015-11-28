2bd3c5f97f75c1fb2381cf9f2165f95fc6c8dc8a
Jhoan <jhoannarango@msn.com>
2015-11-27 20:22:38 -0600

    Initial Commit

---
+++ Teams-Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teams-Up
+//
+//  Created by Jhoan Arango on 11/27/15.
+//  Copyright © 2015 Jhoan Arango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
