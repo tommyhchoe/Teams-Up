7f6af73d1bf23282818f511ce939553700f09f4d
Martin Wildfeuer <mwfire@mwfire.de>
2015-12-12 00:05:32 +0100

    Persist players to NSUserDefaults

--- Teams-Up/Player.swift
+++ Teams-Up/Player.swift
@@ -9,7 +9,20 @@
 import Foundation
 
 
-class Player{
+// Martin Wildfeuer sayz:
+// Using an enum for keys instead of constants
+// might be a bit too much for a class with only
+// two properties. I like this approach though,
+// as it keeps the class itself more readable.
+// This is open for discussion
+
+private enum Keys: String {
+    case Name   = "Name"
+    case Rating = "Rating"
+}
+
+
+class Player: NSObject, NSCoding {
     let name: String
     let rating: Double
     
@@ -17,4 +30,22 @@ class Player{
         self.name = name
         self.rating = rating
     }
+    
+    // MARK: NSCoding compliance
+    // We need this so we can persist our players via NSUserdefaults
+    // NSUserdefaults does not store custom types by default, so we
+    // need to store them as NSData via NSKeyedArchiver
+    // see: PlayersDataSource
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey(Keys.Name.rawValue) as! String
+        rating = aDecoder.decodeDoubleForKey(Keys.Rating.rawValue)
+        super.init()
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: Keys.Name.rawValue)
+        aCoder.encodeDouble(rating, forKey: Keys.Rating.rawValue)
+    }
 }
+
